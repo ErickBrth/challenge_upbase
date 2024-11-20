7efab7fbbd6fff7ebe12e483e786b18c0700deb1
ErickBrth <erickaraken@gmail.com>
2024-11-20 20:30:49 -0300

    fix:adjust card infos

--- challenge_upbase/src/pages/Home/index.jsx
+++ challenge_upbase/src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Container,
   WelcomeText,
@@ -9,10 +9,17 @@ import {
   CardContent,
   CardTitle,
   CardSubtitle,
-  Wrapper
-} from './styles';
-import { RegisterButton } from '../../Components/RegisterButton'
-import { ArrowBack } from '../../Components/ArrowBack';
+  CardPrice,
+  Infos,
+  CardTitleLine,
+  StarIcon,
+  DailyRate,
+  DailyText,
+  Wrapper,
+} from "./styles";
+import { RegisterButton } from "../../Components/RegisterButton";
+import { ArrowBack } from "../../Components/ArrowBack";
+import StarRoundedIcon from '@mui/icons-material/StarRounded';
 
 function Home() {
   return (
@@ -22,22 +29,35 @@ function Home() {
         <RegisterButton />
       </Wrapper>
       <WelcomeText>Bem-vindo, Raphael!</WelcomeText>
-      
+
       <ButtonGroup>
         <Button primary>Todos os flats</Button>
         <Button>Indisponíveis</Button>
       </ButtonGroup>
       <Card>
-        <CardImage src="https://via.placeholder.com/400" alt="Flat" />
-        <CardContent>
-          <CardTitle>Poundsflats Aquário</CardTitle>
-          <CardSubtitle>Santos, SP - Diária: R$97</CardSubtitle>
-        </CardContent>
+        <CardImage
+          src="https://s3-alpha-sig.figma.com/img/9180/3c60/56cde7387c7af8e5d3fad83849a7ed02?Expires=1733097600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hG4qQ5~YrU1afncg6fLjpbGk9Jt0AyRTdtoyA00ZddYT9M8fv~DAU2CmgTa4-DGLwclcNsR4-AIiq-lESvqY-1YSUCNH4UBO7-Aw23YlJmj3rJXTzMVMgdRlcCf9roE5JiOhnhDAH9E6jKtZCpzHa~vbnAnf8-uJlt9WYQKn2neb5y03NJ84hm-N3M-Yh63d5RBUm17IT1KHnia3U3nDrWJxptR~8mQ9CSXGbqOQmxuDpiULAydNM5orYxy7HN8TUZ87mWldlMLDVq8ltdwWi2imXzWpUbTvRALxWNqHmFouLd5uvWpZ6EzakIIFpQ1KIYdCvvxa5rzTtdjn0dHCoQ__"
+          alt="Flat"
+        />
       </Card>
+      <CardContent>
+        <CardTitleLine>
+          <CardTitle>Poundsflats Aquário</CardTitle>
+          <StarIcon>
+            <StarRoundedIcon sx={{color: "#7B2CBF"}}></StarRoundedIcon>
+            4,5
+          </StarIcon>
+        </CardTitleLine>
+        <Infos>
+          <CardSubtitle>Santos, SP</CardSubtitle>
+          <DailyRate>
+            <DailyText>Diária:</DailyText>
+            <CardPrice>R$97</CardPrice>
+          </DailyRate>
+        </Infos>
+      </CardContent>
     </Container>
   );
 }
 
 export default Home;
-
-
--- challenge_upbase/src/pages/Home/styles.js
+++ challenge_upbase/src/pages/Home/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
   padding: 20px;
@@ -26,29 +26,27 @@ export const ButtonGroup = styled.div`
 
 export const Button = styled.button`
   padding: 6px 20px;
-  border: ${(props) => (props.primary ? 'none' : '1px solid #00000029')};
+  border: ${(props) => (props.primary ? "none" : "1px solid #00000029")};
   border-radius: 8px;
   font-size: 12px;
   cursor: pointer;
   text-align: center;
   line-height: 16px;
-  font-family: 'Jura', sans-serif;
+  font-family: "Jura", sans-serif;
 
-  background-color: ${(props) => (props.primary ? '#8000FF' : '#FFF')};
-  color: ${(props) => (props.primary ? '#FFF' : '#000')};
-
+  background-color: ${(props) => (props.primary ? "#8000FF" : "#FFF")};
+  color: ${(props) => (props.primary ? "#FFF" : "#000")};
 
   &:hover {
-    background-color: ${(props) => (props.primary ? '#5a00cc' : '#E0E0E0')};
+    background-color: ${(props) => (props.primary ? "#5a00cc" : "#E0E0E0")};
   }
 `;
 
 export const Card = styled.div`
-  margin-top: 20px;
+  margin-top: 30px;
   width: 100%;
   max-width: 400px;
-  border-radius: 10px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 24px;
   overflow: hidden;
   background-color: #fff;
 `;
@@ -59,17 +57,57 @@ export const CardImage = styled.img`
 `;
 
 export const CardContent = styled.div`
-  padding: 15px;
+  margin-top: 18px;
+`;
+
+export const CardTitleLine = styled.h3`
+  display: flex;
+  justify-content: space-between;
+  margin: 0;
 `;
 
 export const CardTitle = styled.h3`
   font-size: 18px;
+  display: flex;
   color: #333;
-  margin: 0 0 10px 0;
+  margin: 0 0 0 0;
 `;
 
 export const CardSubtitle = styled.p`
-  font-size: 14px;
-  color: gray;
+  font-size: 16px;
+  display: flex;
   margin: 0;
+`;
+export const Infos = styled.p`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 8px;
+  margin-bottom: 0;
+`;
+
+export const CardPrice = styled.p`
+  font-size: 16px;
+  display: flex;
+  margin: 0;
+  font-weight: 400;
+`;
+
+export const StarIcon = styled.span`
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  margin: 0;
+`;
+
+export const DailyRate = styled.h3`
+  font-size: 16px;
+  display: flex;
+  margin: 0;
+  gap: 4px;
+`;
+
+export const DailyText = styled.h3`
+  font-size: 16px;
+  margin: 0;
+  font-weight: bold;
 `;
